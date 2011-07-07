5b352808001ca8eb04a57c231d5b8cb09fffbaf8
Katya Bain <katherina.amelishko@gmail.com>
2011-07-07 12:06:01 -0500

    Finish exercises chapter 9

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -54,11 +54,14 @@ describe "sign in/out" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
-        controller.should be_signed_in
+#  exercse 1 chapter 9 start        
+integration_sign_in(user)
+#        visit signin_path
+ #       fill_in :email,    :with => user.email
+  #      fill_in :password, :with => user.password
+   #     click_button
+# exercise 1 chapter 9 end       
+ controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
       end
